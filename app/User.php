197c56f826acb062bccf6ebf74b39b7be68bebce
sakura <cherry.m-love4@ezweb.ne.jp>
2020-11-04 12:26:14 +0900

    #857 エラーの解消

--- app/User.php
+++ app/User.php
@@ -37,18 +37,5 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    /**
-     * IDから一件のデータを取得する
-     */
-    public function selectUserFindById($id)
-    {
-        // 「SELECT id, name, kana,nickname,email WHERE id = ?」を発行する
-        $query = $this->select([
-            'id','name','nickname','email'
-        ])->where([
-            'id' => $id
-        ]);
-        // first()は1件のみ取得する関数
-        return $query->first();
-    }
+    
 }
