654b5ca0729fc65730b712bf519740d8dba162b8
sakura <cherry.m-love4@ezweb.ne.jp>
2020-11-11 17:31:32 +0900

    #888 簡単な説明とログイン・新規登録ボタンの実装

--- resources/views/contents/top.blade.php
+++ resources/views/contents/top.blade.php
@@ -9,6 +9,32 @@
     <h1>KOKOIKO</h1>
     <p>My favorite places</p>
   </div>
+  <div class="row justify-content-center">
+    <div class="col-md-6 col-lg-3 top-message">
+      <h5 class="mt-2"><i class="fas fa-heart faa-tada animated mr-2" title="お気に入り"></i>大好きな場所を登録しよう</h5>
+      <p class="mb-2">「また行きたい」を覚えておける！</p>
+    </div>
+    <div class="col-md-6 col-lg-3 top-message">
+      <h5 class="mt-2"><i class="fas fa-star faa-tada animated mr-2" title="行ってみたい"></i>気になる場所をリスト化しよう</h5>
+      <p class="mb-2">「行ってみたい」を「行ってみよう」に！</p>
+    </div>
+    <div class="col-md-6 col-lg-3 top-message">
+      <h5 class="mt-2"><i class="far fa-lightbulb faa-tada animated mr-2"></i>旅行先で寄り道しちゃおう</h5>
+      <p class="mb-2">マップで近くのお店をピックアップ！</p>
+    </div>
+    <div class="col-md-6 col-lg-3 top-message">
+      <h5 class="mt-2"><i class="far fa-thumbs-up faa-tada animated mr-2"></i>好きをシェアしよう</h5>
+      <p class="mb-2">みんなのお気に入りを見てみよう！</p>
+    </div>
+    <div class="col-12 top-message">
+      <h5 class="mt-5">さぁ、お出かけしよう！</h5>
+    </div>
+    <div class="col-md-6 top-message">
+      <a href="{{ route('login') }}" class="btn btn-primary d-block mx-auto mt-3 w-50">ログイン</a>
+    </div>
+    <div class="col-md-6 top-message">
+    <a href="{{ route('register') }}" class="btn btn-outline-primary d-block mx-auto mt-3 w-50">新規登録</a>
+  </div>
 </div>
 @endsection
 @section('js')
